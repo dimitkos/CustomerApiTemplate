13f7087389cf286d558259767c15e59f138f5970
dimitkos <33483947+dimitkos@users.noreply.github.com>
2021-12-30 10:16:32 +0200

    Add project files.

---
+++ Shared/Payloads.Customer.cs
@@ -0,0 +1,36 @@
+﻿namespace Shared
+{
+    public class AddCustomer
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public string Email { get; }
+        public string Address { get; }
+
+        public AddCustomer(int id, string name, string email, string address)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Address = address;
+        }
+    }
+
+    public class SearchCustomers
+    {
+        public string? Name { get; }
+        public string? Email { get; }
+        public string? Address { get; }
+        public int? Skip { get; }
+        public int? PageSize { get; }
+
+        public SearchCustomers(string? name = null, string? email = null, string? address = null, int? skip = null, int? pageSize = null)
+        {
+            Name = name;
+            Email = email;
+            Address = address;
+            Skip = skip;
+            PageSize = pageSize;
+        }
+    }
+}
---
+++ Shared/Types.Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    public class Customer
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public string Email { get; }
+        public string Address { get; }
+
+        public Customer(int id, string name, string email, string address)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Address = address;
+        }
+    }
+}
